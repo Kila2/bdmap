6692472ec71c6bc052718ab44cf9be69383137be
李俊良 <lijunliang@wondersgroup.com>
2016-05-31 17:20:33 +0800

    21级特殊处理

--- pnglink/image/rename.bash
+++ pnglink/image/rename.bash
@@ -4,6 +4,17 @@ y=$2
 z=$3
 w=$4
 l=$5
+if [[ $z -eq 21 ]]; then
+	for (( i = 0; i < $w; i++ )); do
+		name1=$(printf "%02d" $(($i + 1)))
+		for ((j=0; j<$l; j++)) 
+		do 
+			name2=$(printf "%02d" $(($j + 1)))
+			#echo 18_$name1`echo _`$name2.jpg
+			mv $z`echo _`$name1`echo _`$name2.jpg $z`echo _`$(($x + $i))`echo _`$(($y - $j)).jpg
+		done
+	done
+else
 for((i=0;i<$w;i++))
 do
 	for ((j=0; j<$l; j++)) 
@@ -13,3 +24,5 @@ do
 		mv $z`echo _`$name.jpg $z`echo _`$(($x + $j))`echo _`$(($y - $i)).jpg
 	done
 done
+fi
+
